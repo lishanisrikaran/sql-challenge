ee00a2e5d9d9fbff46564e20cc21385b01aea0e0
lishanisrikaran <l.srikaran@outlook.com>
2023-06-24 18:08:04 +0100

    Data Engineering Updates

--- EmployeeSQL/2. Data Engineering/Schema.sql
+++ EmployeeSQL/2. Data Engineering/Schema.sql
@@ -2,7 +2,7 @@
 -- Creates departments table.
 
 CREATE TABLE departments (
-	dept_no VARCHAR(5) NOT NULL, 
+	dept_no CHAR(4) NOT NULL, 
 	dept_name VARCHAR(255) NOT NULL, 
 	PRIMARY KEY (dept_no)
 	);
@@ -16,7 +16,7 @@ SELECT * FROM departments;
 -- Creates titles table
 
 CREATE TABLE titles (
-	title_id VARCHAR(5) NOT NULL, 
+	title_id CHAR(5) NOT NULL, 
 	title VARCHAR(255) NOT NULL,
 	PRIMARY KEY (title_id)
 	);
@@ -31,11 +31,11 @@ SELECT * FROM titles;
 
 CREATE TABLE employees (
 	emp_no INTEGER NOT NULL, 
-	emp_title_id VARCHAR(5) NOT NULL, 
+	emp_title_id CHAR(5) NOT NULL, 
 	birth_date DATE NOT NULL, 
 	first_name VARCHAR(255) NOT NULL, 
 	last_name VARCHAR(255) NOT NULL, 
-	sex CHAR NOT NULL, 
+	sex CHAR(1) NOT NULL, 
 	hire_date DATE NOT NULL, 
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id), 
 	PRIMARY KEY (emp_no)
@@ -52,7 +52,7 @@ LIMIT 100;
 
 CREATE TABLE dept_emp (
 	emp_no INTEGER NOT NULL, 
-	dept_no VARCHAR(5) NOT NULL, 
+	dept_no CHAR(4) NOT NULL, 
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no), 
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no), 
 	PRIMARY KEY (emp_no, dept_no)
@@ -68,7 +68,7 @@ LIMIT 100;
 -- Creates dept_manager table.
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(5) NOT NULL, 
+	dept_no CHAR(4) NOT NULL, 
 	emp_no INTEGER NOT NULL, 
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no), 
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no), 
